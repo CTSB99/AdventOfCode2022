40900d8e0c866db29c26acc12ec73e98a2209c25
Christian Schulze <Christian.Schulze@softwareone.com>
2022-12-19 08:52:03 +0100

    day7

--- Day6/ConsoleApp1/ConsoleApp1/Program.cs
+++ Day6/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System.Xml;
+
+string input = System.IO.File.ReadAllText(@"../../../Input.txt");
+int i = 0;
+int k = 0;
+char[] charArray = new char[14];
+bool unique = false;
+
+while (i < input.Length)
+{
+    while(k < 14)
+    {
+        charArray[k] = input[i + k];
+        k++;
+    }
+    k = 0;
+    unique = charArray.Distinct().Count() == 14;
+
+    if (unique)
+    {
+        Console.WriteLine(i+14);
+        break;
+    }
+
+    i++;
+}
